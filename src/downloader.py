9d32ae3babe12fa052216f3ab0e4071b71185336
Mariel Tinaco <tinaco.mariel@gmail.com>
2022-05-01 16:15:54 +0800

    Moved training segment metadata

--- src/downloader.py
+++ src/downloader.py
@@ -1,34 +1,60 @@
 import sys, os
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abstractproperty
 from pytube import YouTube
+from pathlib import Path
+from typing import Any, Union, Protocol
 
 import threading
 
 sys.path.append(os.path.join(os.getcwd(), "."))
 
+from media import Media
 
-class MediaDownload (ABC):
+class YoutubeMediaDownloader (ABC):
 
     @abstractmethod
-    def start (self):
+    def run (self, url_extension : str, output_dir : str, file_extension: str):
         ...
 
+class YoutubeAudioDownloader (YoutubeMediaDownloader):
+
+    def run (self, url_extension : str, output_dir : str, file_extension: str):
+        url = f"http://www.youtube.com/watch?v={url_extension}"
+        try:
+            yt = YouTube(url)
+
+            stream = yt.streams.get_audio_only()
+            stream.download(output_path=output_dir, filename=Path(f"{yt.title}.{file_extension}"))
+
+        except Exception as e:
+            raise e
 
 
-def download(yt_object : YouTube):
+def fetch_audio(media : Media, file_ext : str):
 
-    dlpath = f"audio/"
-
-    stream = yt_object.streams.get_audio_only()
-    stream.download(output_path=dlpath,filename=f"{yt_object.title}.mp3")
+    downloader = YoutubeAudioDownloader()
+    downloader.run(
+        url_extension = media.url_extension,  
+        output_dir = Path("io/media/audio/{}".format(media.tags[0])),
+        file_extension= file_ext )
 
 if __name__ == "__main__":
 
-    yt1 = YouTube("http://www.youtube.com/watch?v=-0xzrMun0Rs")
-    yt2 = YouTube("http://www.youtube.com/watch?v=-0jeONf82dE")
+    media_1 = Media (
+        url_extension="-1PZQg5Gi8A",
+        start= 30.000, 
+        stop=40.000,
+        tags=["Smash, crash"])
 
-    x1 = threading.Thread(target=download, args=(yt1,))
-    x2 = threading.Thread(target=download, args=(yt2,))
+    media_2 = Media (
+        url_extension="-2EKWgTNEYU",
+        start=30.000,
+        stop=40.000,
+        tags=["Radio"]
+    )
+
+    x1 = threading.Thread(target=fetch_audio, args=(media_1, ".mp3"))
+    x2 = threading.Thread(target=fetch_audio, args=(media_2, ".mp3"))
 
     x1.start()
     x2.start()
