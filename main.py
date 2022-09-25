f6456bc293052e96aafc393175c1c4cfd6f1988e
mariel-tinaco <tinaco.mariel@gmail.com>
2022-09-25 14:55:27 +0800

    New video getter

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 
 import json
 from pathlib import Path
+import pandas as pd
 import sys, os
 import pypeln as pl
 import asyncio
+import functools
+
 from tqdm.asyncio import trange, tqdm
 from tqdm import tqdm
 
@@ -12,12 +15,46 @@ from src.datasetloader import DataProcessor, audiosetLoader, extract_media, medi
 from src.downloader import fetch_m4a_audio
 from src.utils.downloader import fullpipe
 
-if __name__ == "__main__":
+
+def get_by_multiple_keywords (keywords : list, source_path):
+
+    processor = DataProcessor()
+
+    filter_keywords = keywords
+    dataset_path = source_path
+
+    with open (Path("io/metadata/mapping.json"), 'r') as jsonfile:
+        mapping = json.load(jsonfile)
+        reversed_mapping = {v:k for k, v in mapping.items()}
+
+    filter_keys = [reversed_mapping[kw] for kw in filter_keywords]
+
+
+    with open (dataset_path, "r") as file:
+        data = processor.load(file, audiosetLoader)
+
+        '''
+        Solution for filtering found here https://pandas.pydata.org/docs/reference/api/pandas.Series.str.contains.html
+        '''        
+        
+        filtered_data_list = [data.loc[data['positive_labels'].str.contains(filter_key, regex=False)] for filter_key in filter_keys]
+
+        reducer = lambda x, y: pd.merge(x, y, how='inner', on=['YTID', 'start_seconds', 'end_seconds', 'positive_labels'])
+
+        filtered_data = functools.reduce(reducer, filtered_data_list)
+
+        generator = media_extractor(filtered_data)
+
+        stage = pl.sync.each (fullpipe, generator, workers=10)
+        pl.sync.run(stage)
+
+
+def get_by_keyword (keyword):
 
     processor = DataProcessor()
     balanced_dataset_path = Path("io/metadata/balanced_train_segments.csv")
 
-    filter_keyword = "Neigh, whinny"
+    filter_keyword = keyword
 
     with open (Path("io/metadata/mapping.json"), 'r') as jsonfile:
         mapping = json.load(jsonfile)
@@ -37,3 +74,11 @@ if __name__ == "__main__":
 
         stage = pl.sync.each (fullpipe, generator, workers=10)
         pl.sync.run(stage)
+
+
+
+if __name__ == "__main__":
+
+    get_by_multiple_keywords (['Horse', 'Cough'], source_path=Path("io/metadata/unbalanced_train_segments.csv"))
+
+    # get_by_keyword("Horse")
